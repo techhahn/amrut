c6cc0d0ea8a4ea5919e4e26613f075ad1a3df127
Pritesh Panjiker <priteshpanjiker@gmail.com>
2014-06-18 15:51:22 +0530

    Removed user registration and added Admin controller

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -8,25 +8,25 @@ class UsersController extends BaseController {
         $this->layout->content = View::make('users.login');
     }
 
-    public function getRegister() {
-        $this->layout->content = View::make('users.create');
-    }
-
-    public function postRegister() {
-        $validator = Validator::make(Input::all(), User::$rules);
-        if($validator->passes()) {
-            $user = new User;
-            $user->username = Input::get('username');
-            $user->firstname = Input::get('firstname');
-            $user->lastname = Input::get('lastname');
-            $user->password = Hash::make(Input::get('password'));
-            $user->save();
-            return Redirect::to('users/login')->with('message', 'Sucessfully account created');
-        }
-        else {
-            return Redirect::to('users/register')->withErrors($validator)->withInput();
-        }
-    }
+//    public function getRegister() {
+//        $this->layout->content = View::make('users.create');
+//    }
+//
+//    public function postRegister() {
+//        $validator = Validator::make(Input::all(), User::$rules);
+//        if($validator->passes()) {
+//            $user = new User;
+//            $user->username = Input::get('username');
+//            $user->firstname = Input::get('firstname');
+//            $user->lastname = Input::get('lastname');
+//            $user->password = Hash::make(Input::get('password'));
+//            $user->save();
+//            return Redirect::to('users/login')->with('message', 'Sucessfully account created');
+//        }
+//        else {
+//            return Redirect::to('users/register')->withErrors($validator)->withInput();
+//        }
+//    }
 
     public function postLogin() {
         $validator = Validator::make(Input::all(), User::$LoginRules);
